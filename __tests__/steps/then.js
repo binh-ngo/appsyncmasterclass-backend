6a75dc7606b1d8fc43777985175519a8cc0cf858
Binh Ngo <bnngo97@gmail.com>
2023-06-08 23:29:40 -0700

    deployed project with revised code

--- __tests__/steps/then.js
+++ __tests__/steps/then.js
@@ -1,22 +1,22 @@
-require('dotenv').config();
-const AWS = require("aws-sdk")
+require('dotenv').config()
+const AWS = require('aws-sdk')
 
-const user_exists_in_UsersTable = async(id) => {
-    const DynamoDB = new AWS.DynamoDB.DocumentClient();
+const user_exists_in_UsersTable = async (id) => {
+  const DynamoDB = new AWS.DynamoDB.DocumentClient()
 
-    console.log(`looking for user [${id}] in table [${process.env.USERS_TABLE}]`);
-    const resp = await DynamoDB.get({
-        TableName: process.env.USERS_TABLE,
-        Key: {
-            id
-        }
-    }).promise()
+  console.log(`looking for user [${id}] in table [${process.env.USERS_TABLE}]`)
+  const resp = await DynamoDB.get({
+    TableName: process.env.USERS_TABLE,
+    Key: {
+      id
+    }
+  }).promise()
 
-    expect(resp.Item).toBeTruthy()
+  expect(resp.Item).toBeTruthy()
 
-    return resp.Item;
+  return resp.Item
 }
 
 module.exports = {
-    user_exists_in_UsersTable
+  user_exists_in_UsersTable
 }
